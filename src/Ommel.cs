965ee237a104fedc292725175e88ef8954a5e359
Zatherz <zatherz@linux.pl>
2019-10-12 22:30:40 +0200

    Config-dependant version

--- src/Ommel.cs
+++ src/Ommel.cs
@@ -17,8 +17,12 @@ namespace Ommel {
 	}
 
 	public class Ommel {
-		public const string VERSION = "0.1-dev";
-		public const string NOITA_VERSION = "mods-beta 1+";
+#if DEBUG
+        public const string VERSION = "0.1-dev";
+#else
+        public const string VERSION = "0.1";
+#endif
+        public const string NOITA_VERSION = "mods-beta 1+";
 		public const string MODS_FOLDER_NAME = "mods";
 		public const string MOD_METADATA_NAME = "mod.xml";
         public const string MOD_OMMELDATA_NAME = "ommel.xml";

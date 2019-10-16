2fd4b7a4f56f708bece209ccfced95b09a25ee4c
Zatherz <zatherz@linux.pl>
2019-10-16 19:09:37 +0200

    Bump version

--- src/Ommel.cs
+++ src/Ommel.cs
@@ -26,9 +26,9 @@ namespace Ommel {
 
 	public class Ommel {
 #if DEBUG
-        public const string VERSION = "0.1.12-dev";
+        public const string VERSION = "0.1.13-dev";
 #else
-        public const string VERSION = "0.1.12";
+        public const string VERSION = "0.1.13";
 #endif
         public const string NOITA_VERSION = "mods-beta 1+";
 		public const string MODS_FOLDER_NAME = "mods";

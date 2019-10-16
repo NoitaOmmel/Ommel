34b37f6ff2cd6e0917f74d2ae681691442f17d22
Zatherz <zatherz@linux.pl>
2019-10-16 16:36:51 +0200

    Update parser

--- src/Ommel.cs
+++ src/Ommel.cs
@@ -12,6 +12,7 @@ using System.Text.RegularExpressions;
 using System.Net;
 using System.Security.Cryptography.X509Certificates;
 using System.Net.Security;
+using System.Security.Cryptography;
 
 namespace Ommel {
 	public static class OmmelMain {
@@ -25,9 +26,9 @@ namespace Ommel {
 
 	public class Ommel {
 #if DEBUG
-        public const string VERSION = "0.1.11-dev";
+        public const string VERSION = "0.1.12-dev";
 #else
-        public const string VERSION = "0.1.11";
+        public const string VERSION = "0.1.12";
 #endif
         public const string NOITA_VERSION = "mods-beta 1+";
 		public const string MODS_FOLDER_NAME = "mods";
